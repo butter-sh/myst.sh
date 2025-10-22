cccc0adabf3bfcaa0b06a5ca07253aa28b5cbd14
Sebastian Krüger <valknar@pivoine.art>
2025-10-22 11:02:54 +0200

    feat: clean.sh final7

--- setup.sh
+++ setup.sh
@@ -10,8 +10,7 @@ command -v jq >/dev/null 2>&1 || {
   echo "Error: jq is required but not installed."
   exit 1
 }
-command -v yq >/dev/null 2>&1 |  \
-  | { echo "Warning: yq is not installed. YAML support will be limited."; }
+command -v yq >/dev/null 2>&1 | { echo "Warning: yq is not installed. YAML support will be limited."; }
 
 # Make main script executable
 chmod +x myst.sh
